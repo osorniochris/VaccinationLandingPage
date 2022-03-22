8928614cfbf2fb3c2103be001e001d506aa0e9df
Christopher Osornio <osorniochris@gmail.com>
2022-03-21 19:54:19 -0600

    Why section and preparations section added

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const nav = document.querySelectorAll(".sidenav");
   const navInstance = M.Sidenav.init(nav, {});
+
+  const collapsible = document.querySelectorAll(".collapsible");
+  const collInstance = M.Collapsible.init(collapsible, {});
 });
